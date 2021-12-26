855dc9e1039b16cbecc0b1312301439e965ce5c2
Etienne Gramlich <etgramlich@gmx.de>
2021-12-26 12:31:30 +0100

    Added option to only output flac files

--- split_ffmpeg.py
+++ split_ffmpeg.py
@@ -57,16 +57,11 @@ def parseChapters(filename):
 
   return chapters
 
-def getChapters():
-  parser = OptionParser(usage="usage: %prog -i filename", version="%prog 1.0")
-  parser.add_option("-i", "--input",dest="infile", help="Input File", metavar="FILE")
-  (options, args) = parser.parse_args()
-  if not options.infile:
-    parser.error('Input file ("-i") required')
-  chapters = parseChapters(options.infile)
-  fbase, fext = os.path.splitext(options.infile)
-  path, file = os.path.split(options.infile)
-  newdir, fext = os.path.splitext( basename(options.infile) )
+def getChapters(infile):
+  chapters = parseChapters(infile)
+  fbase, fext = os.path.splitext(infile)
+  path, file = os.path.split(infile)
+  newdir, fext = os.path.splitext( basename(infile) )
 
   os.mkdir(path + "/" + newdir)
 
@@ -75,7 +70,7 @@ def getChapters():
     chap['name'] = chap['name'].replace("'","\'")
     print "start:" +  chap['start']
     chap['outfile'] = path + "/" + newdir + "/" + re.sub("[^-a-zA-Z0-9_.():' ]+", '', chap['name']) + fext
-    chap['origfile'] = options.infile
+    chap['origfile'] = infile
     print chap['outfile']
   return chapters
 
@@ -99,9 +94,39 @@ def convertChapters(chapters):
       output = e.output
       raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
+def convertChaptersToFlac(chapters, audioIndex=0): # By default use first audio track
+  for chap in chapters:
+    chap['outfile'] = os.path.splitext(chap['outfile'])[0] + '.flac'
+    print "start:" +  chap['start']
+    print chap
+    command = [
+        "ffmpeg", '-i', chap['origfile'],
+        '-vn', # No video output
+        '-c:a', 'flac',
+        '-map', '0:a:' + audioIndex,
+        '-ss', chap['start'],
+        '-to', chap['end'],
+        chap['outfile']]
+    output = ""
+    try:
+      # ffmpeg requires an output file and so it errors when it does not get one
+      output = sp.check_output(command, stderr=sp.STDOUT, universal_newlines=True)
+    except CalledProcessError, e:
+      output = e.output
+      raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
 def main():
-  chapters = getChapters()
-  convertChapters(chapters)
+  parser = OptionParser(usage="usage: %prog -i filename", version="%prog 1.0")
+  parser.add_option("-i", "--input", dest="infile", help="Input File", metavar="FILE")
+  parser.add_option("-a", "--audioStream", dest="audioIndex", help="Audio stream index")
+  (options, args) = parser.parse_args()
+  if not options.infile:
+    parser.error('Input file ("-i") required')
+  chapters = getChapters(options.infile)
+  if not options.audioIndex:
+    convertChapters(chapters)
+  else:
+    convertChaptersToFlac(chapters, options.audioIndex)
 
 if __name__ == '__main__':
   main()
